0c088bffc433afe02b426e545e2d456cf608162c
duynguyen-workspace <duynguyen.workspace@gmail.com>
2023-05-04 21:57:33 +0700

    fix DataOutput class

--- src/main/java/model/data/DataOutput.java
+++ src/main/java/model/data/DataOutput.java
@@ -4,50 +4,69 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
+
 import database.ShoppingDB;
-import model.product.ProductMap;
 import model.cart.ShoppingCart;
+import model.cart.ShoppingCartList;
+import model.product.Product;
+import model.product.ProductMap;
 
 public class DataOutput {
     public void writeReceipt(int cartID) {
         try {
             FileWriter fWriter = new FileWriter("src/main/java/database/receipts/cart"+cartID+".txt");
+            Scanner input = new Scanner(System.in);
 
-            // Get shopping cart
-            ShoppingCart c = ShoppingDB.getInstance().getCarts().getCart(cartID);
+            // Ask for user full name
+            System.out.print("Enter your full name: ");
+            String fullName = input.nextLine();
+
+            // Get shopping carts and products information
+            ShoppingDB db = ShoppingDB.getInstance();
+            ProductMap products = db.getProducts();
+            ShoppingCartList carts = db.getCarts();
+            ShoppingCart c = carts.getCart(cartID);
 
             // Header
-            fWriter.write("\t\t\t\tOnline Shopping Service\n");
-            fWriter.write("\n");
+            fWriter.write("\t\t\t\tOnline Shopping Service\n\n");
             // Receipt No.
-            fWriter.write("Receipt Number: C2440 - G09 - C" + c.getCartID() + "\t\t\t\n");
+            fWriter.write("Receipt Number: C2440 - G09 - C" + cartID + "\t\t\t\n");
 
             // Date & Time
             SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
             Date date = new Date();
             fWriter.write(formatter.format(date)+"\n");
-            fWriter.write("User: Nguyen Anh Duy \n");
-            fWriter.write("---------------------------------------------\n");
-            fWriter.write("ITEMS \t\t\t\t\t\tQUANTITY\t\t\t  PRICE\n");
+            fWriter.write("User: " + fullName + "\n");
+            fWriter.write("------------------------------------------------\n");
+            fWriter.write(String.format("%-40s %s \n","ITEMS","PRICE"));
 
             // Write each item
             for (String item : c.getCartItems().keySet()) {
+                // Get product information
+                Product p = products.getProduct(item);
+
                 int quantity = c.getCartItems().get(item);
-                fWriter.write(item + "\t\t\t\t\t\t\t" + quantity + "\t\t\t\t" + " $1480.00" + "\n");
+                double price = p.getPrice();
+                double subProductTotal = price * quantity;
+
+                // Write product information
+                fWriter.write(item + "\n");
+                fWriter.write(String.format("%d x %-35.2f $%.2f\n",quantity,price,subProductTotal));
+                // fWriter.write(quantity + "x " + String.format("$%,.2f",p.getPrice()) + "\t\t\t\t\t\t\t\t" + subProductTotal + "\n");
                 String coupon = c.getAppliedCouponID();
                 if (coupon != null) {
                     // replace later with product price discount and total price of a product
                     fWriter.write(" *Discount(10%)*" + "\t\t\t\t\t" + "($1480.00)" + "\n");
                 }
             }
-            fWriter.write("---------------------------------------------\n");
+            fWriter.write("------------------------------------------------\n");
             // Cart Items Amount
-            ProductMap products = ShoppingDB.getInstance().getProducts();
-            fWriter.write("Items Count:\t\t" + c.countItems() + "\n\n");
-            fWriter.write("Subtotal: \t\t\t\t\t\t\t" + " $1923.11" + "\n");
-            fWriter.write("Discount: \t\t\t\t\t\t\t" + " -" + "$12.50" + "\n");
-            fWriter.write("Tax: \t\t\t\t\t\t\t\t" + " +" + "$12.50" + "\n\n");
-            fWriter.write("TOTAL: \t\t\t\t\t\t\t\t" + String.format("$%.2f",c.cartAmount(products)) + "\n");
+            fWriter.write("Items Count: " + c.countItems() + "\n\n");
+            fWriter.write("Subtotal: \t\t\t\t\t\t\t\t" + "$1923.11" + "\n");
+            fWriter.write("Discount: \t\t\t\t\t\t\t\t-" + "$12.50" + "\n");
+            fWriter.write("Tax: \t\t\t\t\t\t\t\t\t+" + "$12.50" + "\n\n");
+            fWriter.write("TOTAL: \t\t\t\t\t\t\t\t\t" + String.format("$%.2f",c.cartAmount(products)) + "\n");
             fWriter.flush();
             fWriter.close();
             System.out.println("Finish writing the printing receipt for cart #" + c.getCartID() + "!");
@@ -67,7 +86,6 @@ public class DataOutput {
         // Test data output
         DataOutput dOut = new DataOutput();
         ShoppingDB db = ShoppingDB.getInstance();
-        ShoppingCart c = db.getCarts().getCart(2);
-        dOut.writeReceipt(1);
+        dOut.writeReceipt(2);
     }
 }
